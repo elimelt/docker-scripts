464dfda10e6fc9561ca44e1b7b258515029bd1fc
Elijah Melton <98496503+elimelt@users.noreply.github.com>
2023-12-25 17:44:29 -0800

    Update run-sim.py

--- benchmarks/run-sim.py
+++ benchmarks/run-sim.py
@@ -6,7 +6,7 @@ def generate_docker_compose(num_servers, server_port_start = 3002):
     services = {}
     client_ports = []
 
-    # Generate server services
+    # generate server services
     for port in range(server_port_start, num_servers + server_port_start):
         service_name = f'server{port - server_port_start + 1}'
         port_mapping = f"\"{port}:{3000}\""
@@ -17,9 +17,7 @@ def generate_docker_compose(num_servers, server_port_start = 3002):
             "volumes": ["./server:/app"]
         }
 
-
-
-    # Generate client services
+    # generate client services
     for port in range(4001, 4001 + 5):
         service_name = f'client{port - 4000}'
         port_mapping = f"\"{port}:{3000}\""
@@ -30,7 +28,7 @@ def generate_docker_compose(num_servers, server_port_start = 3002):
             "volumes": ["./client:/app"]
         }
 
-    # Convert services dictionary to yaml format
+    # convert to yaml
     docker_compose_content = "version: '3'\n\nservices:\n"
     for service, config in services.items():
         docker_compose_content += f"  {service}:\n"
@@ -49,25 +47,25 @@ def generate_docker_compose(num_servers, server_port_start = 3002):
 
 
 def run_simulation(num_servers):
-    # Generate Docker Compose content
+    # generate docker-compose content
     docker_compose_content = generate_docker_compose(num_servers)
     
-    # Write to temporary file
+    # write to temp file
     with open("docker-compose-temp.yml", "w") as file:
         file.write(docker_compose_content)
 
-    # Run Docker Compose up
+    # run docker compose up
     subprocess.run(["docker-compose", "-f", "docker-compose-temp.yml", "up", "-d"])
 
-    # Wait for a while (adjust as needed based on the server startup time)
+    # wait for startup
     subprocess.run(["sleep", "20"])
 
-    # Capture logs
+    # capture logs
     result_file = f"results_{num_servers}_servers.txt"
     with open(result_file, "w") as file:
         subprocess.run(["docker-compose", "-f", "docker-compose-temp.yml", "logs"], stdout=file)
 
-    # Stop and remove containers
+    # stop and remove containers
     subprocess.run(["docker-compose", "-f", "docker-compose-temp.yml", "down"])
 
     print(f"Simulation for {num_servers} servers completed. Results saved in {result_file}")
